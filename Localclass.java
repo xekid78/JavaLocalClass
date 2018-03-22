9ae1bd6370d2d09e1ea8a50c9e3ea4957062b85b
Makoto Tanabe <xekid78@outlook.com>
2018-03-22 12:24:06 +0900

    Add files via upload

---
+++ Localclass.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class Localclass {
+
+	public static void main(String[] args) {
+
+		class Msg {
+			public void HelloWorld() {
+				System.out.println("Hello World");
+			}
+
+		}
+
+		Msg msg = new Msg();
+		msg.HelloWorld();
+
+	}
+
+}
